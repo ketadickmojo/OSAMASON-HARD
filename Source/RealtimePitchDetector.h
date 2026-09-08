7aa2fe2c4978c4ff81cd3c0de1f1eb9e84d4b145
ketadickmojo <5d2syrjf5p@privaterelay.appleid.com>
2026-09-08 17:21:56 +0300

    Update RealtimePitchDetector.h

--- Source/RealtimePitchDetector.h
+++ Source/RealtimePitchDetector.h
@@ -5,19 +5,15 @@
 class RealtimePitchDetector
 {
 public:
-    void prepare (
-        double newSampleRate);
-
+    void prepare(double newSampleRate);
     void reset();
 
-    float process (
-        float input);
+    float process(float input);
 
 private:
     double sampleRate = 44100.0;
 
     float lastPitch = 0.0f;
-
     float previousSample = 0.0f;
     float previousDifference = 0.0f;
 
