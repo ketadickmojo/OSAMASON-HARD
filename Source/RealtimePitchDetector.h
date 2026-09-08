906cf63d849af9fedc0868f04805dfaa094d5ef0
ketadickmojo <5d2syrjf5p@privaterelay.appleid.com>
2026-09-08 17:33:21 +0300

    Update RealtimePitchDetector.h

--- Source/RealtimePitchDetector.h
+++ Source/RealtimePitchDetector.h
@@ -2,16 +2,31 @@
 
 #include <juce_audio_basics/juce_audio_basics.h>
 
+#include <array>
+#include <cmath>
+
 class RealtimePitchDetector
 {
 public:
     void prepare(double newSampleRate);
     void reset();
 
-    float process(float input);
+    float process(float sample);
 
 private:
+    static constexpr int bufferSize = 2048;
+    static constexpr int analysisHop = 64;
+
+    float detectPitch();
+
     double sampleRate = 44100.0;
+
+    std::array<float, bufferSize> buffer {};
+
+    int writeIndex = 0;
+    int samplesSinceAnalysis = 0;
+
+    float smoothedPitch = 0.0f;
 
     float lastPitch = 0.0f;
     float previousSample = 0.0f;
