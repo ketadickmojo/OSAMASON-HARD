ff4150ed03bf4850a68d67e7bdc537a47856bc6c
ketadickmojo <5d2syrjf5p@privaterelay.appleid.com>
2026-09-08 17:15:39 +0300

    Update AutoTuneEngine.h

--- Source/AutoTuneEngine.h
+++ Source/AutoTuneEngine.h
@@ -1,88 +1,56 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
+
 #include "RealtimePitchDetector.h"
+
+#include <array>
+#include <cmath>
 
 class AutoTuneEngine
 {
 public:
     enum class ScaleType
     {
-        Major = 0,
+        Major,
         Minor,
         Chromatic
     };
 
     void prepare (
-        double sampleRate,
-        int samplesPerBlock);
+        double newSampleRate,
+        int newSamplesPerBlock);
 
     void reset();
 
-    void setEnabled (
-        bool shouldBeEnabled);
-
-    void setKey (
-        int newKey);
-
-    void setScale (
-        ScaleType newScale);
-
-    void setRetuneSpeed (
-        float newSpeed);
-
-    void setAmount (
-        float newAmount);
+    void setEnabled (bool shouldBeEnabled);
+    void setKey (int newKey);
+    void setScale (ScaleType newScale);
+    void setRetuneSpeed (float newSpeed);
+    void setAmount (float newAmount);
 
     void process (
         juce::AudioBuffer<float>& buffer);
 
 private:
-    double sampleRate = 48000.0;
-    int blockSize = 128;
-
-    bool enabled = false;
-
-    int key = 0;
-
-    ScaleType scale =
-        ScaleType::Major;
-
-    float retuneSpeed = 50.0f;
-    float amount = 100.0f;
-
-    RealtimePitchDetector detector;
-
-    float currentPitch = 0.0f;
-    float targetPitch = 0.0f;
-
-    float correctionSemitones = 0.0f;
-    float smoothedCorrection = 0.0f;
-
     static constexpr int delaySize = 8192;
-
-    std::array<float, delaySize> delayL {};
-    std::array<float, delaySize> delayR {};
-
-    int delayWritePosition = 0;
-
-    float oscillatorPhase = 0.0f;
 
     float processSample (
         float input,
         bool rightChannel);
 
-    float getTargetMidiNote (
-        float detectedMidi) const;
-
-    bool isNoteAllowed (
-        int midiNote) const;
+    float frequencyToMidi (
+        float frequency) const;
 
     float midiToFrequency (
         float midi) const;
 
-    float frequencyToMidi (
-        float frequency) const;
+    bool isNoteAllowed (
+        int midiNote) const;
+
+    float getTargetMidiNote (
+        float detectedMidi) const;
 
     float semitoneDistance (
         float from,
@@ -93,4 +61,31 @@ private:
     float readDelay (
         const std::array<float, delaySize>& delay,
         float position) const;
+
+    RealtimePitchDetector detector;
+
+    double sampleRate = 44100.0;
+    int blockSize = 128;
+
+    bool enabled = false;
+
+    int key = 0;
+
+    ScaleType scale = ScaleType::Major;
+
+    float retuneSpeed = 50.0f;
+    float amount = 100.0f;
+
+    std::array<float, delaySize> delayL {};
+    std::array<float, delaySize> delayR {};
+
+    int delayWritePosition = 0;
+
+    float currentPitch = 0.0f;
+    float targetPitch = 0.0f;
+
+    float correctionSemitones = 0.0f;
+    float smoothedCorrection = 0.0f;
+
+    float oscillatorPhase = 0.0f;
 };
